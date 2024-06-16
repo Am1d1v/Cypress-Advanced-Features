fbcecb5fed3e16a0e4272d6a573a4828d3996233
Am1d1v <dimadiman1adima@gmail.com>
2024-06-16 19:57:36 +0300

    Contact form Test Suite

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -11,11 +11,20 @@ describe('Contact form', () => {
         // Select name input field then insert data
         cy.get('[data-cy="contact-input-name"]').type('UserName');
 
-        // Select name input field then insert data
+        // Select email input field then insert data
         cy.get('[data-cy="contact-input-email"]').type('UserEmail@gmail.com');
 
         // Select submit button then click it
+        cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
+        cy.get('[data-cy="contact-btn-submit"]').should('not.have.attr', 'disabled');
         cy.get('[data-cy="contact-btn-submit"]').click();
+        
+        // Select submit button and check that it is sending inputed data
+        cy.get('[data-cy="contact-btn-submit"]').contains('Sending');
+
+        // Select submit button and check that it is disabled while sending data
+        cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled');
+
     });
 
 });
