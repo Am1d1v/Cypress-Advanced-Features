4ac9a8edd7d2a5b022a8897bf8839b61dab30d5f
Am1d1v <dimadiman1adima@gmail.com>
2024-06-17 12:41:05 +0300

    Cypress Simulating Special Key Presses

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -2,7 +2,7 @@
 
 describe('Contact form', () => {
 
-    it('should submit the form', () => {
+    it('should submit the form clicking on "Send Message" button', () => {
         cy.visit('http://localhost:5173/about');
 
         // Select message input field then insert data
@@ -15,13 +15,34 @@ describe('Contact form', () => {
         cy.get('[data-cy="contact-input-email"]').type('UserEmail@gmail.com');
 
         // Select submit button then click it
-        /* cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
+        cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
         cy.get('[data-cy="contact-btn-submit"]').should('not.have.attr', 'disabled');
-        cy.get('[data-cy="contact-btn-submit"]').click(); */
-        cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+        cy.get('[data-cy="contact-btn-submit"]').click();
+        /* cy.get('[data-cy="contact-btn-submit"]').then((el) => {
             expect(el.attr('disabled')).to.be.undefined
             expect(el.text()).to.contain('Send Message');
-        });
+        }); */
+
+        
+        // Select submit button and check that it is sending inputed data
+        cy.get('[data-cy="contact-btn-submit"]').contains('Sending');
+
+        // Select submit button and check that it is disabled while sending data
+        cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled');
+
+    });
+
+    it.only('should submit the form pressing "Enter"', () => {
+        cy.visit('http://localhost:5173/about');
+
+        // Select message input field then insert data
+        cy.get('[data-cy="contact-input-message"]').type('Message Text');
+
+        // Select name input field then insert data
+        cy.get('[data-cy="contact-input-name"]').type('UserName');
+
+        // Select email input field then insert data
+        cy.get('[data-cy="contact-input-email"]').type('UserEmail@gmail.com{enter}');
 
         
         // Select submit button and check that it is sending inputed data
